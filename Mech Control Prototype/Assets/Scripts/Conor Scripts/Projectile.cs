990b9c2c584635d0158c9cd65eca791314b6e25f
MonkeyZebraProductions <73033054+MonkeyZebraProductions@users.noreply.github.com>
2022-11-27 19:24:36 +0000

    Camera fix

--- Mech Control Prototype/Assets/Scripts/Conor Scripts/Projectile.cs
+++ Mech Control Prototype/Assets/Scripts/Conor Scripts/Projectile.cs
@@ -8,7 +8,7 @@ public class Projectile : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(this.gameObject,2f);
     }
 
     // Update is called once per frame
